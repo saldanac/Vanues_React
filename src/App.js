bee90d6523e9e673d64992749d20085b20c7677f
User <user@MacBook-Air-User-2.local>
2018-10-27 17:45:04 +0400

    finish Highlights section

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './resources/styles.css'
 
 import Header from './components/header_footer/Header'
 import Featured from './components/featured'
-import VanueNFo from "./components/vanueNFo";
+import VanueNFo from "./components/vanueNFo"
+import Highlights from './components/Highlights'
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: '1500px', background: 'cornflowerblue' }}>
+      <div className="App" style={{ height: '1500px'}}>
         <Header/>
         <Featured/>
         <VanueNFo/>
+        <Highlights/>
       </div>
     );
   }
---
+++ src/components/Highlights/Descriptions.js
@@ -0,0 +1,17 @@
+ import React from 'react';
+ import Fade from 'react-reveal/Fade'
+ 
+ const Descriptions = () => {
+   return (
+     <Fade>
+       <div className='center_wrapper'>
+          <h2>Highlights</h2>
+          <div className='highlight_description'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.Fugiat quasi, impedit aspernatur facere ratione rerum voluptatibus nisi odit provident illo.Dolor totam inventore obcaecati magnam, corrupti eius illum recusandae suscipit ut illo, iusto omnis aliquid quas ipsam cupiditate.Quis, in ?
+          </div>
+       </div>
+     </Fade>
+   );
+ };
+ 
+ export default Descriptions;
